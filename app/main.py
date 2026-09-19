21fe6eb198226614a2e1cf94138c4bb58da6c869
kevinmwright <kevin.m.wright@outlook.com>
2026-09-19 18:40:33 +0200

    Updated

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 class SoftwareEngineer:
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.skills = []
+    def __init__(self, name: str) -> None:        
+        if not hasattr(self, 'name'):
+            self.name = name
+        if not hasattr(self, 'skills'):
+            self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
@@ -10,7 +12,7 @@ class SoftwareEngineer:
 class FrontendDeveloper(SoftwareEngineer):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills.extend(["HTML", "CSS", "JavaScript"])
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
     def create_awesome_web_page(self) -> str:
         print("{name} is creating a webpage...".format(name=self.name))
